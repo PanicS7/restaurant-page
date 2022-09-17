a684e0677d24760562151f6f90eef7ee7f1d612f
Sanja <gamesg274@gmail.com>
2022-09-17 22:30:40 +0200

    Styled footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 function Footer() {
   
   const footer = `
-    <footer>
+    <footer id="footer">
       <p>Developed by <a href="https://github.com/PanicS7" target="_blank">Sanja Panic</a></p>
     </footer>
   `;
